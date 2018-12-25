50fc7f0501b268f1e6e933f23ca23ec5dde4e026
veia-mac <917971502@qq.com>
2018-12-25 23:32:29 +0800

    add swagger

---
+++ src/main/java/xyz/veiasai/util/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package xyz.veiasai.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableWebMvc
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("xyz.veiasai.hibernate.controller")) // 注意修改此处的包名
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("接口列表 v1.1.0") // 任意，请稍微规范点
+                .description("接口测试") // 任意，请稍微规范点
+                .termsOfServiceUrl("http://master/bookstore/swagger-ui.html") // 将“url”换成自己的ip:port
+                .contact("laowu") // 无所谓（这里是作者的别称）
+                .version("1.1.0")
+                .build();
+    }
+}
